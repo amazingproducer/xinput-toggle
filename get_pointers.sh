20df48992db4610e9bf703a9a92110bb5545f90d
amazingproducer <amazingproducer@gmail.com>
2021-06-26 22:57:22 +0000

    investigate basic persistence

--- get_pointers.sh
+++ get_pointers.sh
@@ -10,5 +10,17 @@ unset ar_xpointer_ids
 unset ar_xpointer_names
 readarray -t ar_xpointer_ids < <(jq '.[]["id"]' /tmp/xinput_toggle_targets)
 readarray -t ar_xpointer_names < <(jq '.[]["name"]' /tmp/xinput_toggle_targets)
-echo ${ar_xpointer_ids[@]}
-echo ${ar_xpointer_names[@]}
+#echo ${ar_xpointer_ids[@]}
+#echo ${ar_xpointer_names[@]}
+xi_check=$(ls /tmp/xinput_toggle_target 2> /dev/null) && existing_target=1 || existing_target=0
+if [ $existing_target -eq 0 ]
+then
+echo "Target input device not found; please select:"
+select pointer in "${ar_xpointer_names[@]}"; do
+read xpointer_id < <(jq .[$(($REPLY-1))]."id" /tmp/xinput_toggle_targets)
+echo "$pointer, ID=$xpointer_id targeted."
+echo $xpointer_id > /tmp/xinput_toggle_target
+done
+else
+echo "Current target input: $(cat /tmp/xinput_toggle_target)"
+fi
